d454a6f72acbf9e661eb02b67a4417f053ebac41
kungumabrindha002 <38215062+kungumabrindha002@users.noreply.github.com>
2018-04-10 18:16:23 +0530

    Create alphabets.py

---
+++ alphabets.py
@@ -0,0 +1,6 @@
+v=('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z')
+h=input("character:")
+if h in v:
+    print("alphabets")
+else:
+    print("no")
