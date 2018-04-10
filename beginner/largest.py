25535d980920ba1c0f43afa140dc083ec3cfab24
kungumabrindha002 <38215062+kungumabrindha002@users.noreply.github.com>
2018-04-10 18:34:02 +0530

    Create largest.py

---
+++ beginner/largest.py
@@ -0,0 +1,9 @@
+a=input("enter a value:")
+b=input("enter a value:")
+c=input("enter a value:")
+if(a>b and a>c):
+    print("a is larger")
+elif(b>c and b>a):
+     print("b is larger")
+else:
+     print("c is larger")
